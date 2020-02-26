11f586cf38fbd2a8fa25f3a4956f0f1919b739a1
zty200329 <1412306160@qq.com>
2020-02-26 14:58:00 +0800

    2020226

---
+++ src/main/java/com/uchain/vo/PicVO.java
@@ -0,0 +1,17 @@
+package com.uchain.vo;
+
+import lombok.Data;
+
+/**
+ * @Author: LZH
+ * @Date: 2020/2/24 下午9:01
+ * @Description:
+ */
+@Data
+public class PicVO {
+    private Integer uploaded;
+
+    private String fileName;
+
+    private String url;
+}
